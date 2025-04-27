0dd9813c7b72e574882244711d403d10c724bd14
Krishna Phanish <kskphanish10@gmail.com>
2025-04-28 00:11:01 +0530

    second

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 const app = express();
 app.use(
   cors({
-    origin: "https://chopurl.onrender.com",
+    origin: "https://trustero.onrender.com/",
   })
 );
 app.use(express.json());
